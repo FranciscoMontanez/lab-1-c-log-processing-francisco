9a6aa9b6da03b82e064e6d28cacb1491da10ae54
Francisco <francisco.montanez96@gmail.com>
2017-09-07 22:41:00 -0500

    this is the process_client_logs.sh. It works. will comment on code later.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/bash
-
-dir=$1
-
-# cd $dir && cat $dir > failed_login_data.txt
-
-# cat $dir > failed_login_data.txt
+#!/bin/bash
+cd $1
+ls ./var/log | cat ./var/log/* | grep -E "Failed" | awk '{print $1, $2, $3, $9, $11, $13}' | sed -E 's/:[0-9]+:[0-9]+//g' | sed -E 's/invalid //g' | awk '{for(i=6;i<=6;i++)$i="";$1=$1;$2=$2;$3=$3;$4=$4;$5=$5}1'  > failed_login_data.txt
