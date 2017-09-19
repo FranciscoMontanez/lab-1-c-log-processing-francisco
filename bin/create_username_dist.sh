a16c4a675c8f9e077c8baec309d079f01da039c7
malikdanishkhan <malik083@morris.umn.edu>
2017-09-19 17:10:29 -0500

    Adding comments to the file for clarity

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 path=$(pwd)
+
 cd $1
+
+#Using for loop to obtain data from failed_login_data.txt in each subdirectory
+#and printing userNames using awk then merging
 for subfolder in */ ; do
          cd "$subfolder"
          sort -k 4  failed_login_data.txt | awk '{print $4}' > result.txt        
@@ -8,11 +13,20 @@ for subfolder in */ ; do
          rm result.txt
          cd ..
 done;
+
+#Sorts the data on userNameResult.txt onto usernameResult.txt
 sort  userNameResult.txt |uniq -c | awk '{print "data.addRow([""\x27"$2"\x27"",",$1"]);"}' > usernameResult.txt
+
+#Removing unsorted temp file
 rm userNameResult.txt
+
+#Moving sorted temp file to directory
 mv usernameResult.txt ..
 cd ..
+
+#Wrapping contents of usernameResult.txt to create html file
 wrap_contents.sh usernameResult.txt html_components/username_dist $path/username_dist.html
+
 rm usernameResult.txt
 
 
